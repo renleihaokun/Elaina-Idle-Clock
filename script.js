72577e32955cc4cfb1fdcbfc7151ef755d90b667
renleihaokun <chaogeshou520@163.com>
2026-04-07 21:11:54 +0800

    添加全屏按钮

--- script.js
+++ script.js
@@ -495,8 +495,30 @@ function updateBattery() {
     }
 }
 
+function toggleFullScreen() {
+    if (!document.fullscreenElement) {
+        if (document.documentElement.requestFullscreen) {
+            document.documentElement.requestFullscreen();
+        } else if (document.documentElement.webkitRequestFullscreen) {
+            document.documentElement.webkitRequestFullscreen();
+        } else if (document.documentElement.msRequestFullscreen) {
+            document.documentElement.msRequestFullscreen();
+        }
+    } else {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.webkitExitFullscreen) {
+            document.webkitExitFullscreen();
+        } else if (document.msExitFullscreen) {
+            document.msExitFullscreen();
+        }
+    }
+}
+
 window.togglePanel = togglePanel;
 window.searchLocation = searchLocation;
 window.autoLocate = autoLocate;
 window.fetchWitchQuote = fetchWitchQuote;
+window.toggleFullScreen = toggleFullScreen;
+
 window.addEventListener('load', init);
